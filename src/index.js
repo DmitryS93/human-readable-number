d4075a0ff7456178c9e22cd8a7b97b65943990fa
DmitryS93 <119356125+DmitryS93@users.noreply.github.com>
2023-01-11 17:00:56 +0300

    Update: fix mistakes

--- src/index.js
+++ src/index.js
@@ -5,28 +5,35 @@ module.exports = function toReadable (number) {
 
     let result = 0;
 
-    if (number < 9) {
+    if (number <= 9 && number >= 1) {
         result = hundredsAndOnes[number];
-        return result;
-    } else if (number >= 10 && number < 20) {
+    } else if (number === 0) {
+        result = 'zero';
+    } else if (number >= 10 && number <20) {
         const numStr = String(number);
         result = tenToNineteen[numStr[1]];
-        return result;
-    } else if (number >= 21 && number < 100) {
+    } else if (number >= 20 && number < 100) {
         const numStr = String(number);
-        result = dozens[numStr[0]] + " " + hundredsAndOnes[numStr[1]];
-        return result;
+        if (numStr[1] === "0") {
+            result = dozens[numStr[0]];
+        } else {
+            result = dozens[numStr[0]] + " " + hundredsAndOnes[numStr[1]];
+        }
     } else {
         const numStr = String(number);
-        if (numStr[1] === "1") {
+        if (numStr[1] === "0" && numStr[2] === "0") {
+            result = hundredsAndOnes[numStr[0]] + " hundred";
+        } else if (numStr[2] === "0" && numStr[1] === "1") {
+            result = hundredsAndOnes[numStr[0]] + " hundred ten";
+        } else if (numStr[2] === "0") {
+            result = hundredsAndOnes[numStr[0]] + " hundred " + dozens[numStr[1]];
+        } else if (numStr[1] === "1") {
             result = hundredsAndOnes[numStr[0]] + " hundred " + tenToNineteen[numStr[2]];
-            return result;
         } else if (numStr[1] === "0") {
             result = hundredsAndOnes[numStr[0]] + " hundred " + hundredsAndOnes[numStr[2]];
-            return result;
         } else {
             result = hundredsAndOnes[numStr[0]] + " hundred " + dozens[numStr[1]] + " " + hundredsAndOnes[numStr[2]];
-            return result;
         }    
     } 
+return result
 }
